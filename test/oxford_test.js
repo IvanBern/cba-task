6da000b5d6d3ee8293d88535b8fa1a94b1e11a28
Ivan Bernikov <ivanb@IB-MBP.local>
2020-09-26 22:57:09 +1000

    add eslint and resolve lint errors

--- test/oxford_test.js
+++ test/oxford_test.js
@@ -1,66 +1,61 @@
-var chakram = require('chakram'),
-    expect = chakram.expect;
+const chakram = require('chakram');
+
+const { expect } = chakram;
 
 function getOfxordAPI(resource) {
-    const baseUrl = 'https://od-api.oxforddictionaries.com/api/v2'
-    const app_id = '125f978a';
-    const app_key = 'd6309c7c230ea785d4565e5d289dd0a1';
+  const baseUrl = 'https://od-api.oxforddictionaries.com/api/v2';
+  const appID = '125f978a';
+  const appKey = 'd6309c7c230ea785d4565e5d289dd0a1';
 
-    var response = chakram.get(baseUrl + resource, {
-        headers: {
-            "Accept": "application/json",
-            "app_id": app_id,
-            "app_key": app_key
-        }
-    });
-    return response
+  const response = chakram.get(baseUrl + resource, {
+    headers: {
+      Accept: 'application/json',
+      app_id: appID,
+      app_key: appKey,
+    },
+  });
+  return response;
 }
 
-function getWordOrigin(word_id) {
-    return getOfxordAPI("/entries/en-gb/" + word_id);
-};
-
-function getTranslation(source_lang_translate, target_lang_translate, word_id) {
-    const resource = "/translations/" + source_lang_translate + "/" + target_lang_translate + "/" + word_id;
-    return getOfxordAPI(resource);
+function getWordOrigin(wordID) {
+  return getOfxordAPI(`/entries/en-gb/${wordID}`);
 }
 
+function getTranslation(sourceLangTranslate, targetLangTranslate, wordID) {
+  const resource = `/translations/${sourceLangTranslate}/${targetLangTranslate}/${wordID}`;
+  return getOfxordAPI(resource);
+}
 
-describe("Origin of the word API Tests", function () {
+describe('Origin of the word API Tests', () => {
+  it('origin - Insurance - 200', () => {
+    expect(getWordOrigin('Insurance')).to.have.status(200);
+    return chakram.wait();
+  });
 
-    it("origin - Insurance - 200", function () {
-        expect(getWordOrigin("Insurance")).to.have.status(200);
-        return chakram.wait();
-    });
+  it('origin - word not found - 404', () => {
+    expect(getWordOrigin('NotExistentWord')).to.have.status(404);
+    return chakram.wait();
+  });
 
-
-    it("origin - word not found - 404", function () {
-        expect(getWordOrigin("NotExistentWord")).to.have.status(404);
-        return chakram.wait();
-    });
-
-
-    it("origin - Unknown filter value - 400", function () {
-        expect(getWordOrigin("Insurance?lexicalCategory=test")).to.have.status(400);
-        return chakram.wait();
-    });
+  it('origin - Unknown filter value - 400', () => {
+    expect(getWordOrigin('Insurance?lexicalCategory=test')).to.have.status(400);
+    return chakram.wait();
+  });
 });
 
-describe("Translation API Tests", function () {
+describe('Translation API Tests', () => {
+  it('Test Translation from En to Fr - 200', () => {
+    expect(getTranslation('en', 'fr', 'Test')).to.have.status(200);
+    return chakram.wait();
+  });
 
-    it("Test Translation from En to Fr - 200", function () {
-        expect(getTranslation('en', 'fr', 'Test')).to.have.status(200);
-        return chakram.wait();
-    });
+  it('Test Translation from En to Fr - 404', () => {
+    expect(getTranslation('en', 'fr', 'NotExistentWord')).to.have.status(404);
+    return chakram.wait();
+  });
 
-
-    it("Test Translation from En to Fr - 404", function () {
-        expect(getTranslation('en', 'fr', 'NotExistentWord')).to.have.status(404);
-        return chakram.wait();
-    });
-
-    it("Test Translation from En to Fr - 400", function () {
-        expect(getTranslation('xx', 'fr', 'Test')).to.have.status(200);
-        return chakram.wait();
-    });
+  it('Test Translation from En to Fr - 400', () => {
+    expect(getTranslation('xx', 'fr', 'Test')).to.have.status(200);
+    return chakram.wait();
+  });
 });
